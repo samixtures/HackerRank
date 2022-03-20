3cafa1c44ca738cb1a15c67ebf0055e6281abf96
samixtures <samibajwa@csu.fullerton.edu>
2022-03-19 22:31:45 -0700

    day10 Passed some test cases

---
+++ 30 days of code Python/day10BinaryNumbers.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+    list = []
+        #convert decimal to binary using recursion?
+    while n > 0:
+        remainder = n%2
+        list.append(remainder)
+        n = int(n/2)
+    # print(list)
+    counter = 0
+    max_counter = 0
+    for x in list:
+        if x == 1:
+            counter+=1
+        elif x == 0:
+            if counter > max_counter:
+                max_counter = counter
+            counter = 0
+    print(max_counter)
+        
