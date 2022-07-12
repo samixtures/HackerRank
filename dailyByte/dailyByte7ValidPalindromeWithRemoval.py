de5222f1c4af6819637dc10264ea1fa1465d88e3
samixtures <samibajwa@csu.fullerton.edu>
2022-07-12 12:36:50 -0700

    Figured out valid palindrome with removal

--- dailyByte/dailyByte7ValidPalindromeWithRemoval.py
+++ dailyByte/dailyByte7ValidPalindromeWithRemoval.py
@@ -20,25 +20,49 @@
 # dumb dumb dumb dumb dumb
 
 test = "foobof" #foboof
+rev = test[::-1]
+# i = 0
+# temp = test[:i] + test[(1+i):]
+# revTemp = temp[::-1]
+# print("Temp is:", temp)
+# print("revTemp is:", revTemp)
+
+def vpwr(test):
+    i = 0
+    for x in test:
+        if test == rev:
+            return True
+        temp = test[:i] + test[(1+i):]
+        revTemp = temp[::-1]
+        print("Temp is:", temp)
+        print("revTemp is:", revTemp)
+        if temp == revTemp:
+            return True
+        i+=1
+    return False
+print(vpwr(test))
+
+
+    
 
 #iterate and check each character of both words
 #if the program finds one of the letters isn't the same
 #then increment count and skip that letter in reversed word
 #if count is <= 1 then return True, else False
 
-def VPWR(str):
-    temp = str[::-1] #reversed str is now temp
-    count = 0
-    i = 0 #word forwards
-    j = 0 #word backwards
-    for x in str:
-        if str[i] == temp[j]:
-            print(str[i], temp[j])
-            i+=1
-            j+=1
-        elif str[i] != temp[j]:
-            i += 1
-            count += 1
-    return count
+# def VPWR(str):
+#     temp = str[::-1] #reversed str is now temp
+#     count = 0
+#     i = 0 #word forwards
+#     j = 0 #word backwards
+#     for x in str:
+#         if str[i] == temp[j]:
+#             print(str[i], temp[j])
+#             i+=1
+#             j+=1
+#         elif str[i] != temp[j]:
+#             i += 1
+#             count += 1
+#     return count
 
-print(VPWR(test))
+# print(VPWR(test))
