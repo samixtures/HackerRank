130cd1298fa8bb46cd8bcf8f00717da66c4e5a32
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-08-07 15:14:15 -0700

    ✅ Validate Characters passes all tests

--- dailyByte/dailyByte21ValidateCharacters.py
+++ dailyByte/dailyByte21ValidateCharacters.py
@@ -7,3 +7,27 @@
 # "(){}[]", return true
 # "(({[]}))", return true
 # "{(})", return false
+
+s = "(){}[]"
+s1 = "(({[]}))"
+s2 = "{(})"
+
+def validate_characters(s):
+    h = {'(':')', '{':'}', '[':']'}
+    l = []
+    for i in s:
+        print("i is ", i)
+        if i == '(' or i == '{' or i == '[':
+            l.append(i)
+        if len(l) > 0:
+            if l[-1] in h:
+                if h[l[-1]] == i:
+                    print(h[l[-1]])
+                    l.pop(-1)
+            # if i == ')' or i == '}' or i == ']':
+            #     l.pop(-1)
+    print("List is ", l)
+    if not l:
+        return True
+    return False
+print(validate_characters(s))
