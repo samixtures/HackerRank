ca6db8c5c32853eca0bca016e75293196fd998fd
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-08-21 09:12:31 -0700

    ✅ 🕵🏼‍♂️ tracked the moving average

--- dailyByte/dailyByte26MovingAverage.py
+++ dailyByte/dailyByte26MovingAverage.py
@@ -1,4 +1,5 @@
-# This question is asked by Microsoft. Design a class, MovingAverage, which contains a method, next that is responsible for returning the moving average from a stream of integers.
+# This question is asked by Microsoft. Design a class, MovingAverage, which contains 
+# a method, next that is responsible for returning the moving average from a stream of integers.
 # Note: a moving average is the average of a subset of data at a given point in time.
 
 # Ex: Given the following series of events...
@@ -31,3 +32,34 @@
 # }
 
 # }
+
+# // i.e. the moving average has a capacity of 3.
+# MovingAverage movingAverage = new MovingAverage(3);
+# m.next(3) returns 3 because (3 / 1) = 3
+# m.next(5) returns 4 because (3 + 5) / 2 = 4 
+# m.next(7) = returns 5 because (3 + 5 + 7) / 3 = 5
+# m.next(6) = returns 6 because (5 + 7 + 6) / 3 = 6
+
+class MovingAverage:
+    def __init__(self, size):
+        self.size = size
+        self.l = []
+    def get_avg(self):
+        return int(sum(self.l)/len(self.l))
+    def next(self, val):
+        if len(self.l) < self.size:
+            self.l.append(val)
+        else:
+            self.l.pop(0)
+            self.l.append(val)
+        return self.get_avg()
+    def displ(self):
+        print(self.l)
+
+m = MovingAverage(3)
+m.next(3)
+m.next(5)
+m.next(7)
+m.next(6)
+m.displ()
+print(m.get_avg())
