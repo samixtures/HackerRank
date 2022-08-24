9d3d1efd8ba117a6b16095f73571cc18af199a12
samixtures <samibajwa@csu.fullerton.edu>
2022-08-23 17:59:53 -0700

    ❌ working on it

---
+++ dailyByte/dailyByte30LowestCommonAncestor.py
@@ -0,0 +1,42 @@
+# Given a binary search tree that contains unique values 
+# and two nodes within the tree, a, and b, return their lowest common ancestor.
+# Note: the lowest common ancestor of two nodes is 
+# the deepest node within the tree such that both nodes are descendants of it.
+
+# Ex: Given the following tree...
+#        7
+#       / \
+#     2    9
+#    / \ 
+#   1   5 
+# and a = 1, b = 9, return a reference to the node containing 7.
+# Ex: Given the following tree...
+
+#         8
+#        / \
+#       3   9
+#      / \ 
+#     2   6
+# and a = 2, b = 6, return a reference to the node containing 3.
+# Ex: Given the following tree...
+
+#         8
+#        / \
+#       6   9
+# and a = 6, b = 8, return a reference to the node containing 8.
+
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+def trav(t):
+    h = t
+    if not h:
+        return
+    print(h.val)
+    trav(h.left)
+    trav(h.right)
+t = TreeNode(7, TreeNode(2, TreeNode(1), TreeNode(5)), TreeNode(9))
+
+trav(t)
