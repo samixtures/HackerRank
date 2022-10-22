927a72e0da0502a3a88c526ddaa58b5848fb3258
Sami Bajwa <samibajwa@csu.fullerton.edu>
2022-10-21 18:08:47 -0700

    working on it

---
+++ Python/pickingNumbers.py
@@ -0,0 +1,20 @@
+a = [1, 1, 2, 2, 4, 4, 5, 5] # answer: 5 since [4,4,5,5,5,]
+b = [4, 6, 5, 3, 3, 1] # answer: 3 since [4, 3, 3]
+c = [] # answer 0
+d = [5, 2, 4, 6, 1]
+e = [1, 2, 2, 3, 1, 2] # answer 5 since [1, 2, 2, 1, 2]
+def pickingNumbers(a):
+    # traverse array/list
+    # create an array
+    # index = 0, put all the values that are = to it, or greater than it by 1
+    # get the length of the array
+    # put all the values htat are = to it or less than it by 1
+    # get the lenght of that array?
+    # count how many values are less than it by 1 and hwo many values are
+    # greater than it by 1
+    # 
+    # if some value is not = to it or greater by 1, then
+    # add the current array into a new array? But we still need to check
+    # for if there are values that are = or greater than it by 1
+    # it's going to be slower than O(n) for sure then...
+    pass
