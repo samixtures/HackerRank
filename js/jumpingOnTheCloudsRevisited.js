66886bdde71348f8deb4bcff6659d032b2815c2b
samixtures <samibajwa@csu.fullerton.edu>
2023-02-10 17:13:47 -0800

    Eh ❌✅

---
+++ js/jumpingOnTheCloudsRevisited.js
@@ -0,0 +1,60 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the jumpingOnClouds function below.
+function jumpingOnClouds(c, k) {
+    let e = 100;
+    let curr = 0;
+    let thunderCount = 0;
+    while (curr < c.length-1) {
+        curr += k;
+        e -= 1;
+        if (c[curr] == 1) {
+            thunderCount += 1;
+        }
+    }
+    e -= (thunderCount*2);
+    return e;
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = parseInt(nk[0], 10);
+
+    const k = parseInt(nk[1], 10);
+
+    const c = readLine().split(' ').map(cTemp => parseInt(cTemp, 10));
+
+    let result = jumpingOnClouds(c, k);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
