88185c8ac8e7f00416d8d8b4baa6369e70c67717
Sami Bajwa <samibajwa@csu.fullerton.edu>
2023-02-28 13:05:44 -0800

    Kinda did these

---
+++ dailyByte/palindromeSplitting.py
@@ -0,0 +1,12 @@
+# This question is asked by Google. 
+# Given a string s, return all possible partitions of 
+# s such that each substring is a palindrome.
+
+# Ex: Given the following string s…
+
+# s = "abcba",
+# return [
+#     ["a","b","c","b","a"],
+#     ["a","bcb","a"],
+#     ["abcba"]
+# ]
