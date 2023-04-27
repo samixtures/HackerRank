e87a910183b21d6f1320c1f725c4735ae000fb56
Sami Bajwa <samibajwa@csu.fullerton.edu>
2023-04-27 15:58:28 -0700

    ✅

--- dailyByte/palindromeSplitting.py
+++ dailyByte/palindromeSplitting.py
@@ -10,3 +10,31 @@
 #     ["a","bcb","a"],
 #     ["abcba"]
 # ]
+
+test = "abcba" # returns [["a","b","c","b","a"], ["a","bcb","a"], ["abcba"]]
+
+def solution(s):
+    """
+    This is quite interesting. Take a string, split it into elements in a newly made list
+    and those elements must be a palindrome.
+
+    Let's first create a function that checks if a list is a palindrome yea?
+    
+    And let's create some test cases.
+    """
+    palTest1 = "tacocat" # True
+    palTest2 = "robert" # False
+    palTest3 = "Flamingo" # False
+    palTest4 = "racecar" # True
+
+    palTest1 = [x for x in palTest1]
+    palTest2 = [x for x in palTest2]
+    palTest3 = [x for x in palTest3]
+    palTest4 = [x for x in palTest4]
+    # We need to convert the test strings to lists where each character is an element in the string
+    # to do that we can use list comprehension I think, butI kind of forgot how that works
+
+
+    def isPalindrome(str):
+        l, r = 0, len(str)-1
+        
