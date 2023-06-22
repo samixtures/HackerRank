cfcd26d853a43e31a4fdda872f8ed0b38689665d
samixtures <samibajwa@csu.fullerton.edu>
2023-06-21 23:58:38 -0700

    ✅

---
+++ Python/iceCreamParlor.py
@@ -0,0 +1,52 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'icecreamParlor' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts following parameters:
+#  1. INTEGER m
+#  2. INTEGER_ARRAY arr
+#
+
+def icecreamParlor(m, arr):
+    # Write your code here
+    dict = {}
+    target = m
+    
+    for i in range(len(arr)):
+        
+        complement = target - arr[i]
+        
+        if complement in dict:
+            return [dict[complement]+1, i+1]
+        
+        dict[arr[i]] = i
+    
+    
+    return []
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        m = int(input().strip())
+
+        n = int(input().strip())
+
+        arr = list(map(int, input().rstrip().split()))
+
+        result = icecreamParlor(m, arr)
+
+        fptr.write(' '.join(map(str, result)))
+        fptr.write('\n')
+
+    fptr.close()
